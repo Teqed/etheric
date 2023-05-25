6c9a50126026c22e5cdde3ea6602769dcb2bd9af
Timothy Quilling <teqed.arken@gmail.com>
2023-05-25 13:28:03 -0400

    cleanup

--- scripts/monsters/monster.gd
+++ scripts/monsters/monster.gd
@@ -3,13 +3,13 @@ class_name Monster extends CharacterBody2D
 
 var resources := Monster_Resources.new()
 var statpanel_node = preload("res://scenes/combat/statbar.tscn").instantiate()
-var dummymonster_node = preload("res://scenes/adventure/characters/DummyMonster.tscn").instantiate()
+# var dummymonster_node = preload("res://scenes/adventure/characters/DummyMonster.tscn").instantiate()
 var deltaAccumulate = 0
 func _init():
 	add_child(statpanel_node)
-	add_child(dummymonster_node)
-	dummymonster_node.scale = Vector2(4, 4)
-	dummymonster_node.position = Vector2(32, 32)
+	# add_child(dummymonster_node)
+	# dummymonster_node.scale = Vector2(4, 4)
+	# dummymonster_node.position = Vector2(32, 32)
 # func _process(delta):
 # 	#  acculmulate delta
 # 	deltaAccumulate += delta
