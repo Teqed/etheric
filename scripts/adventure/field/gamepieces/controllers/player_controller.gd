39eed0486082ed02943ae02bc21b0274a1f1c88b
Timothy Quilling <teqed.arken@gmail.com>
2023-05-27 23:58:23 -0400

    add pushing

--- scripts/adventure/field/gamepieces/controllers/player_controller.gd
+++ scripts/adventure/field/gamepieces/controllers/player_controller.gd
@@ -7,6 +7,7 @@ extends PassiveAiController
 const GROUP_NAME: = "_PLAYER_CONTROLLER_GROUP"
 
 var is_pushing = false
+var is_slowed = false
 
 func _ready() -> void:
 	# gdlint:ignore = private-method-call
@@ -16,53 +17,50 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	if not _focus.is_moving():
-		var move_dir: = _get_move_direction()
-		if move_dir:
-			var target_cell: = Vector2i.ZERO
-
-			# Unless using 8-direction movement, one movement axis must be preferred.
-			#	Default to the x-axis.
-			if not is_zero_approx(move_dir.x):
-				move_dir = Vector2(move_dir.x, 0)
-			else:
-				move_dir = Vector2(0, move_dir.y)
-
-			_focus.direction = move_dir
-			target_cell = _focus.cell + Vector2i(move_dir)
-
-			# If there is a gamepiece at the target cell, do not move on top of it.
-			_update_changed_cells()
-			if not is_cell_blocked(target_cell) and \
-					not FieldEvents.did_gp_move_to_cell_this_frame(target_cell):
-				var move_path: = pathfinder.get_path_cells(_focus.cell, target_cell)
-
-				# Path is invalid. Bump animation?
-				if move_path.size() <= 1:
-					pass
-
+			# The player is not moving, so check for input.
+			var move_dir: = _get_move_direction()
+			if move_dir:
+				# The player is pressing a movement key or button.
+				var target_cell: = Vector2i.ZERO
+				# Unless using 8-direction movement, one movement axis must be preferred.
+				#	Default to the x-axis.
+				if not is_zero_approx(move_dir.x):
+					move_dir = Vector2(move_dir.x, 0)
 				else:
-					_focus.travel_to_cell(target_cell)
-			else:
-				if not is_pushing:
-					is_pushing = true
-					var parent: Gamepiece = get_parent()
-					parent.move_speed = parent.move_speed / 3
-
-					# Get the gamepiece at the target cell.
-					_target = get_gamepieces_at_cell(target_cell)[0]
-					if interaction():
+					move_dir = Vector2(0, move_dir.y)
+				_focus.direction = move_dir
+				target_cell = _focus.cell + Vector2i(move_dir)
+				# If there is a gamepiece at the target cell, do not move on top of it.
+				_update_changed_cells()
+				if not is_cell_blocked(target_cell) and \
+						not FieldEvents.did_gp_move_to_cell_this_frame(target_cell):
+					var move_path: = pathfinder.get_path_cells(_focus.cell, target_cell)
+					# Path is invalid. Bump animation?
+					if move_path.size() <= 1:
 						pass
 					else:
-						# The player is is_pushing against a wall or another gamepiece.
-						# Let's play the push animation by signalling the animation.
-						_focus.push_begun.emit()
-					_target = null
-		else:
-			if is_pushing:
-				_focus.push_ended.emit()
-				is_pushing = false
-				var parent: Gamepiece = get_parent()
-				parent.move_speed = parent.move_speed * 3
+						_focus.travel_to_cell(target_cell)
+						if is_pushing:
+							var parent: Gamepiece = get_parent()
+							is_slowed = true
+							parent.move_speed = parent.move_speed / 3
+
+				else:
+					if not is_pushing:
+						# Get the gamepiece at the target cell.
+						_target = get_gamepieces_at_cell(target_cell)[0]
+						if interaction():
+							pass
+						else:
+							# The player is is_pushing against a wall or another gamepiece.
+							# Let's play the push animation by signalling the animation.
+							is_pushing = true
+							_focus.push_begun.emit()
+						_target = null
+			else:
+				if is_pushing:
+					_focus.push_ended.emit()
+					is_pushing = false
 
 
 func _get_move_direction() -> Vector2:
@@ -73,6 +71,12 @@ func _get_move_direction() -> Vector2:
 
 
 func _on_focus_arriving(excess_distance: float) -> void:
+	var parent: Gamepiece = get_parent()
+	if is_pushing and is_slowed:
+		parent.move_speed = parent.move_speed * 3
+		_focus.push_ended.emit()
+		is_pushing = false
+		is_slowed = false
 	# gdlint:ignore = private-method-call
 	super._on_focus_arriving(excess_distance)
 	var move_direction: = _get_move_direction()
